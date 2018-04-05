b0d43d56982a6c53a74786e3251471875798c6f4
Matt Rider <mattwr18@gmail.com>
2018-04-05 16:27:59 -0300

    Add methods to ruby_intro.rb

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,33 +3,62 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+  arr.inject(0, :+)
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  if arr.empty?
+    return 0
+  elsif
+    arr.length == 1
+    return arr[0]
+  else
+    return arr.max(2).reduce(:+)
+  end
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  len = arr.length-1
+  for i in 0..len
+    for j in (i + 1)..len
+      if arr[i] + arr[j] == n
+        return true
+      end
+    end
+  end
+    return false
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  return "Hello, #{name}"
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+   /^[^aeiou\W]/i.match(s) != nil
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
-end
+    s == "0" or
+    /^[10]*00$/.match(s) != nil
+  end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+  attr_accessor :isbn, :price
+
+  def initialize(isbn, p)
+    if isbn.length > 0 && p > 0
+      @isbn = isbn
+      @price = p
+    else
+      raise ArgumentError
+    end
+  end
+
+  def price_as_string
+    "$%0.2f" % @price
+  end
 end
